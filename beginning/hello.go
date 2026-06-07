d00bdaa427651840f5e6f21a50f8b09d3fbdb5d2
Antony <antony@hellchildgames.co.uk>
2026-06-07 15:02:03 +0200

    hello me

--- beginning/hello.go
+++ beginning/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const helloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+	if name == "" {
+		name = "World"
+	}
+	return fmt.Sprintf("%s%s!", helloPrefix, name)
 }
 
 func main() {
--- beginning/hello_test.go
+++ beginning/hello_test.go
@@ -3,9 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Antony")
-	want := "Hello, Antony!"
+	t.Run("Saying Hello to people", func(t *testing.T) {
+		got := Hello("Antony")
+		want := "Hello, Antony!"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Saying 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
